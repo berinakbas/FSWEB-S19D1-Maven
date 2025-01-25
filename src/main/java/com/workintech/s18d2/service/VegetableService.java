5ca872d3702cdbd062085932e6eb5ce054a1a2e8
berinakbas <berinakbass@gmail.com>
2025-01-25 23:07:33 +0300

    son

---
+++ src/main/java/com/workintech/s18d2/service/VegetableService.java
@@ -0,0 +1,13 @@
+package com.workintech.s18d2.service;
+
+import com.workintech.s18d2.entity.Vegetable;
+import java.util.List;
+
+public interface VegetableService {
+    List<Vegetable> getByPriceAsc();
+    List<Vegetable> getByPriceDesc();
+    Vegetable getById(long id);
+    Vegetable save(Vegetable vegetable);
+    Vegetable delete(long id);
+    List<Vegetable> searchByName(String name);
+}
